b35f329d3bd1ecd9d0c7e0189868229f7a472ea5
meilihao <563278383@qq.com>
2020-09-16 16:28:19 +0800

    + rtslib iscsi

---
+++ target/rtslib_iscsi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from rtslib import *
+
+# Setup an IBLOCK backstore
+backstore = IBlockBackstore(0, mode='create')
+
+try:
+        storage_object = IBlockStorageObject(backstore, "sdb", "/dev/sdb", gen_wwn=True)
+except:
+        backstore.delete()
+            raise
+
+        # Create an iSCSI target endpoint using an iSCSI IQN
+        fabric = FabricModule('iscsi')
+        target = Target(fabric, "iqn.2003-01.org.linux-iscsi.x.x8664:sn.d3d8b0500fde")
+        tpg = TPG(target, 1)
+
+        # Setup a network portal
+        portal = NetworkPortal(tpg, "192.168.1.128", "3260")
+
+        # Export LUN
+        lun0 = tpg.lun(0, storage_object, "lun")
