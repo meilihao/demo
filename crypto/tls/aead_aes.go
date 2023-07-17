25a9a7703d601d6976dbb2a14e844c5669b41306
meilihao <563278383@qq.com>
2023-07-17 22:31:46 +0800

    u aes-gcm

--- crypto/tls/aead_aes.go
+++ crypto/tls/aead_aes.go
@@ -1,4 +1,15 @@
 // https://github.com/SimonWaldherr/golang-examples/blob/master/advanced/aesgcm.go
+// https://go.dev/src/crypto/cipher/example_test.go
+// [Ciphertext and tag size and IV transmission with AES in GCM mode](https://crypto.stackexchange.com/questions/26783/ciphertext-and-tag-size-and-iv-transmission-with-aes-in-gcm-mode)
+// [Why AES-256 with GCM adds 16 bytes to the ciphertext size?](https://stackoverflow.com/questions/67028762/why-aes-256-with-gcm-adds-16-bytes-to-the-ciphertext-size)
+/*
+aes-gcm:
+- plaintext len limit see `aesgcm.Seal()`
+- Output size = input size
+- len(nonce)=12, Any value other than 12-byte is processed with GHASH
+- len(tag)=16,  GCM is defined for the tag sizes 128, 120, 112, 104, or 96, 64 and 32. Note that the security of GCM is strongly dependent on the tag size.
+- output=Nonce|ciphertext|tag
+*/
 package main
 
 import (
@@ -10,6 +21,7 @@ import (
 )
 
 // AesGcmEncrypt takes an encryption key and a plaintext string and encrypts it with AES256 in GCM mode, which provides authenticated encryption. Returns the ciphertext and the used nonce.
+// openssl_encrypt
 func AesGcmEncrypt(key []byte, plaintext string) (ciphertext, nonce []byte) {
 	plaintextBytes := []byte(plaintext)
 
@@ -18,25 +30,26 @@ func AesGcmEncrypt(key []byte, plaintext string) (ciphertext, nonce []byte) {
 		panic(err.Error())
 	}
 
-	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
-	nonce = make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
+	nonce = make([]byte, aesgcm.NonceSize()) // 12
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
 	ciphertext = aesgcm.Seal(nil, nonce, plaintextBytes, nil)
-	fmt.Printf("Ciphertext: %x\n", ciphertext)
+	fmt.Printf("Ciphertext: %x, %d -> %d\n", ciphertext, len(plaintextBytes), len(ciphertext))
 	fmt.Printf("Nonce: %x\n", nonce)
 
 	return
 }
 
 // AesGcmDecrypt takes an decryption key, a ciphertext and the corresponding nonce and decrypts it with AES256 in GCM mode. Returns the plaintext string.
+// openssl_decrypt
 func AesGcmDecrypt(key, ciphertext, nonce []byte) (plaintext string) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -60,7 +73,7 @@ func AesGcmDecrypt(key, ciphertext, nonce []byte) (plaintext string) {
 }
 
 func main() {
-	// Generate an encryption key. 16 bytes = AES-128, 32 bytes = AES-256.
+	// Generate an encryption key. 16 bytes = AES-128, 24 bytes = AES-192, 32 bytes = AES-256.
 	key := make([]byte, 32)
 	if _, err := io.ReadFull(rand.Reader, key); err != nil {
 		panic(err.Error())
---
+++ crypto/tls/go.mod
@@ -0,0 +1,7 @@
+module tls
+
+go 1.21rc3
+
+require golang.org/x/crypto v0.11.0
+
+require golang.org/x/sys v0.10.0 // indirect
